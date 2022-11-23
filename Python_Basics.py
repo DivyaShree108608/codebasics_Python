efcbf659f5f173316ee68818f5835bc8492c2b01
DivyashreeHS <divyasandeephegde@gmail.com>
2022-11-23 17:40:25 +0530

    Number Completed

---
+++ Python_Basics.py
@@ -0,0 +1,20 @@
+# NUMBER
+#Remember the format() function we used in the last video?? Now pass 145 and ‘o’ in the function and see what it returns.
+# Try to find out which representation it is.
+a =format(145,'o')
+print (a)
+
+# There is a circular pond in a village. This pond has a radius of 84 meters. Can you find the area of the pond?
+# (Bonus: If there is exactly 1.4 liter of water in a square meter, what is the total amount of water in the pond?
+
+r = 84
+area = 3.14*(r**2)
+print(area)
+
+# If you cross a 490-meter-long street in 7 minutes, then what is your speed in meters per second?
+# Print your answer without any decimal point in it.
+
+Distance = 490
+Time = (7*60)
+Speed = round(Distance/Time)
+print(Speed)
